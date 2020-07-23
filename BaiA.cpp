cd6ef2a456090eb9d7f195c26f040ec4a7a38d18
tritanngo99 <49904863+tritanngo99@users.noreply.github.com>
2020-07-23 21:06:25 +0700

    Update BaiA.cpp

--- BaiA.cpp
+++ BaiA.cpp
@@ -1,1 +1,17 @@
 
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    int a =0;
+    int b=1;
+    for(int i=1;i<n;++i)
+    {
+    int tmp=b;
+    b=a+b;
+    a=tmp;
+    }
+    cout<<a<<" "<<b;
+}
