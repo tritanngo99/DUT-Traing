7a96e62e0c1daee15b31ba986b09db084bbf124d
tritanngo99 <49904863+tritanngo99@users.noreply.github.com>
2020-07-31 06:55:38 +0700

    Create BaiA_So_hang_xom.cpp

---
+++ BaiA_So_hang_xom.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long 
+ll a,b,c;
+int main(){
+    cin>>a>>b>>c;
+    if(a%c==b%c) cout<<"YES\n";
+    else cout<<"NO\n";
+    return 0;
+}
